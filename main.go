5633841997731af3aa162979addfc25606435669
Jose Manuel Peso <jmpeso@gmail.com>
2016-12-28 17:32:10 +0100

    simplify naming and folder structure

--- main.go
+++ main.go
@@ -2,12 +2,38 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 
-	"github.com/trilopin/godinary/server"
+	"github.com/julienschmidt/httprouter"
+	"github.com/trilopin/godinary/imagejob"
 )
 
 func main() {
 	port := flag.Int("port", 3001, "Port to listen to")
 	flag.Parse()
-	server.StartServer(*port)
+	StartServer(*port)
+}
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome to Homepage!\n")
+}
+
+func Fetch(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	img := imagejob.ImageJob{}
+	_ = img.New(ps.ByName("info")[1:])
+	_ = img.Download()
+	_ = img.Process(w)
+	w.Header().Set("Content-Type", "image/"+img.TargetFormat)
+}
+
+func StartServer(port int) {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/v0.1/:account/fetch/*info", Fetch)
+	fmt.Println("Listening on port", port)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), router))
 }

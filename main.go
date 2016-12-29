9e017f2d673a80161efa7321f943e04e09f58fe2
Jose Manuel Peso <jmpeso@gmail.com>
2016-12-30 00:07:41 +0100

    remove index and make fetch anonymous

--- main.go
+++ main.go
@@ -14,26 +14,16 @@ import (
 func main() {
 	port := flag.Int("port", 3001, "Port to listen to")
 	flag.Parse()
-	StartServer(*port)
-}
-
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome to Homepage!\n")
-}
-
-func Fetch(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-
-	img := imagejob.ImageJob{}
-	_ = img.New(ps.ByName("info")[1:])
-	_ = img.Download()
-	_ = img.Process(w)
-	w.Header().Set("Content-Type", "image/"+img.TargetFormat)
-}
-
-func StartServer(port int) {
 	router := httprouter.New()
-	router.GET("/", Index)
-	router.GET("/v0.1/:account/fetch/*info", Fetch)
-	fmt.Println("Listening on port", port)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), router))
+
+	router.GET("/v0.1/fetch/*info", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		img := imagejob.ImageJob{}
+		_ = img.New(ps.ByName("info")[1:])
+		_ = img.Download()
+		_ = img.Process(w)
+		w.Header().Set("Content-Type", "image/"+img.TargetFormat)
+	})
+
+	fmt.Println("Listening on port", *port)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), router))
 }

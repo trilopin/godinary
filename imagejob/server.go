0eadcd4c357d709cfebfcbea8060d8c1fdeee8f0
Jose Manuel Peso <jmpeso@gmail.com>
2017-10-06 12:14:29 +0200

    camel case variable acceptHeader

--- imagejob/server.go
+++ imagejob/server.go
@@ -41,8 +41,8 @@ func Fetch(w http.ResponseWriter, r *http.Request) {
 	}
 	urlInfo := strings.Replace(r.URL.Path, "/image/fetch/", "", 1)
 	job := NewImageJob()
-	acceptheader, ok := r.Header["Accept"]
-	job.AcceptWebp = ok && strings.Contains(acceptheader[0], "image/webp")
+	acceptHeader, ok := r.Header["Accept"]
+	job.AcceptWebp = ok && strings.Contains(acceptHeader[0], "image/webp")
 
 	if err := job.Parse(urlInfo); err != nil {
 		raven.CaptureErrorAndWait(err, nil)

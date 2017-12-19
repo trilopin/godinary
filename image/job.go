eea0fe13a1aa37463a1200d7abb6654f3bd0c897
José Manuel Peso <trilopin@users.noreply.github.com>
2017-12-19 16:14:40 +0100

    reestructure parse function, split parseFilters, parseFormat and parseCrop (#41)
    * reestructure parse function, split parseFilters, parseFormat and parseCrop
    
    * tests for parsefilters and parsecrop
    
    * tests refactor
    
    * split filter-url detection from parsing, tests split also

--- image/job.go
+++ image/job.go
@@ -11,12 +11,29 @@ import (
 	bimg "gopkg.in/h2non/bimg.v1"
 )
 
+// Hasher interface
+type Hasher interface {
+	Hash(s string) string
+}
+
+// Sha256 hasher
+type Sha256 struct {
+}
+
+// Hash return a sha256 hash os input string
+func (hasher *Sha256) Hash(s string) string {
+	ht := sha256.New()
+	ht.Write([]byte(s))
+	return hex.EncodeToString(ht.Sum(nil))
+}
+
 // Job manages image transformation
 type Job struct {
 	Source     Image
 	Target     Image
 	Filters    map[string]string
 	AcceptWebp bool
+	Hasher     Hasher
 }
 
 // NewJob constructs a default empty struct and return a pointer to it
@@ -25,84 +42,107 @@ func NewJob() *Job {
 	job.Filters = make(map[string]string)
 	job.Filters["crop"] = "scale"
 	job.Target.Format = bimg.JPEG
+	job.Hasher = &Sha256{}
 	return &job
 }
 
-// Parse creates a Job struct from string
-func (job *Job) Parse(fetchData string) error {
-	var offset int
+func parseFormat(format string, acceptWebp bool) (bimg.ImageType, error) {
+	switch format {
+	case "jpg", "jpeg":
+		return bimg.JPEG, nil
+	case "webp":
+		return bimg.WEBP, nil
+	case "auto":
+		if acceptWebp {
+			return bimg.JPEG, nil // WEBP disabled
+		}
+		return bimg.JPEG, nil
+	case "png":
+		return bimg.PNG, nil
+	case "gif":
+		return bimg.GIF, nil
+	default:
+		return bimg.JPEG, fmt.Errorf("format \"%s\" not allowed", format)
+	}
+}
+
+func parseCrop(crop string) (string, error) {
+	allowed := map[string]bool{
+		"limit": true,
+		"fit":   true,
+		"scale": true,
+	}
+	if !allowed[crop] {
+		return "scale", fmt.Errorf("crop \"%s\" not allowed", crop)
+	}
+	return crop, nil
+}
+
+func (job *Job) parseFilters(s string) error {
 	var err error
-
-	parts := strings.SplitN(fetchData, "/", 2)
-	if len(parts) > 1 && parts[0] != "http:" && parts[0] != "https:" {
-		filters := strings.Split(parts[0], ",")
-		for _, v := range filters {
-			filter := strings.Split(v, "_")
-			switch filter[0] {
-			case "h":
-				job.Target.Height, err = strconv.Atoi(filter[1])
-				if err != nil {
-					return fmt.Errorf("targetHeight is not integer: %v", err)
-				}
-			case "w":
-				job.Target.Width, err = strconv.Atoi(filter[1])
-				if err != nil {
-					return fmt.Errorf("targetWidth is not integer: %v", err)
-				}
-			case "q":
-				job.Target.Quality, err = strconv.Atoi(filter[1])
-				if err != nil {
-					return fmt.Errorf("quality is not integer: %v", err)
-				}
-			case "f":
-				switch filter[1] {
-				case "jpg", "jpeg":
-					job.Target.Format = bimg.JPEG
-				case "webp":
-					job.Target.Format = bimg.WEBP
-				case "auto":
-					if job.AcceptWebp {
-						job.Target.Format = bimg.JPEG // WEBP disabled
-					} else {
-						job.Target.Format = bimg.JPEG
-					}
-				case "png":
-					job.Target.Format = bimg.PNG
-				case "gif":
-					job.Target.Format = bimg.GIF
-				default:
-					return fmt.Errorf("format \"%s\" not allowed", filter[1])
-				}
-
-			case "c":
-				allowed := map[string]bool{
-					"limit": true,
-					"fit":   true,
-					"scale": true,
-				}
-				if !allowed[filter[1]] {
-					return fmt.Errorf("crop \"%s\" not allowed", filter[1])
-				}
-				job.Filters["crop"] = filter[1]
+	filters := strings.Split(s, ",")
+	for _, v := range filters {
+		filter := strings.Split(v, "_")
+		switch filter[0] {
+		case "h":
+			if job.Target.Height, err = strconv.Atoi(filter[1]); err != nil {
+				return fmt.Errorf("targetHeight is not integer: %v", err)
+			}
+		case "w":
+			if job.Target.Width, err = strconv.Atoi(filter[1]); err != nil {
+				return fmt.Errorf("targetWidth is not integer: %v", err)
+			}
+		case "q":
+			if job.Target.Quality, err = strconv.Atoi(filter[1]); err != nil {
+				return fmt.Errorf("quality is not integer: %v", err)
+			}
+		case "f":
+			if job.Target.Format, err = parseFormat(filter[1], job.AcceptWebp); err != nil {
+				return err
+			}
+		case "c":
+			if job.Filters["crop"], err = parseCrop(filter[1]); err != nil {
+				return err
 			}
 		}
-		offset = len(parts[0]) + 1
 	}
-	job.Source.URL, _ = url.QueryUnescape(fetchData[offset:])
-	// Temporary hack until complete rework of parsing
-	if strings.Count(job.Source.URL, "/") == 1 {
-		parts = strings.Split(job.Source.URL, "/")
-		job.Source.URL = parts[1]
+	return nil
+}
+
+// parseURL takes url data and returns ImageID (source URL or file),
+// filters and maybe an error
+func parseURL(s string, isFetch bool) (string, string, error) {
+	var offset int
+	var err error
+	var filters, URL string
+
+	if isFetch {
+		parts := strings.SplitN(s, "/", 2)
+		if parts[0] != "http:" && parts[0] != "https:" {
+			filters = parts[0]
+			offset = len(parts[0]) + 1
+		}
+		URL, err = url.QueryUnescape(s[offset:])
+	} else {
+		parts := strings.Split(s, "/")
+		URL, err = url.QueryUnescape(parts[len(parts)-1])
+		if len(parts) > 1 && strings.Count(parts[0], "_") > 0 {
+			filters = parts[0]
+		}
 	}
+	return filters, URL, err
+}
 
-	ht := sha256.New()
-	ht.Write([]byte(fetchData + string(job.Target.Format)))
-	job.Target.Hash = hex.EncodeToString(ht.Sum(nil))
-
-	hs := sha256.New()
-	hs.Write([]byte(job.Source.URL))
-	job.Source.Hash = hex.EncodeToString(hs.Sum(nil))
-
+// Parse creates a Job struct from string
+func (job *Job) Parse(fetchData string, isFetch bool) error {
+	var err error
+	var filters string
+	filters, job.Source.URL, err = parseURL(fetchData, isFetch)
+	if err = job.parseFilters(filters); err != nil {
+		return err
+	}
+	job.Target.Hash = job.Hasher.Hash(fetchData + string(job.Target.Format))
+	job.Source.Hash = job.Hasher.Hash(job.Source.URL)
 	return nil
 }
 

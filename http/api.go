fc036e6f99a79a4dc62fa9b4116af1fceea137c2
Antoni Florit Homar <toniflorithomar@gmail.com>
2018-09-04 15:56:03 +0200

    delete options

--- http/api.go
+++ http/api.go
@@ -55,9 +55,6 @@ func APIUpload(opts *ServerOpts) func(http.ResponseWriter, *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		if r.Method == "OPTIONS" {
-			return
-		}
 		if r.Method != "POST" {
 			writeErr(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
